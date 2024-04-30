a27833ae8165f6b4f2726740bae73a431c2b596e
Azay961 <ajay023.chaudhary@gmail.com>
2024-04-30 22:54:47 +0545

    Update

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
 
+from langchain_community.document_loaders import PyPDFLoader
+
+from langchain_text_splitters import NLTKTextSplitters
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+from langchain_community.vectorstores import chroma
 
 app = Flask(__name__)
 
@@ -17,11 +24,24 @@ def search_in_docs():
     if not file:
         return render_template("index.html", error="Select File or Input url first")
                                 
+    files = read_file(file=file)
+    db_chroma = create_embedding_store(files)
+
     return render_template("search.html", api_key=api_key, doc=file)
 
 
-def create_embedding_store(file, key):
-    
+def read_file(file):
+    loader = PyPDFLoader(file)
+    pages = loader.load_and_split()
+
+    return pages
+
+embedding_model = GoogleGenerativeAIEmbeddings(google_api_key=key,
+                                               model="models/embeddings-001")
+def create_embedding_store(files, key):
+    text_spltter = NLTKTextSplitters(chunk_size=500, chunk_overlap=100)
+    chunks = text_spltter.split_documents(files)
+
 
 
 if __name__ == "__main__":
